69209ca1bb84a2bf0166c93f61580d008c4630d2
Craig Phares <craigphares@sixoverground.com>
2016-11-17 21:30:19 -0500

    more refactoring

--- app/services/tang/update_customer.rb
+++ app/services/tang/update_customer.rb
@@ -6,11 +6,7 @@ module Tang
       if customer.stripe_id.present?
         begin
           c = Stripe::Customer.retrieve(customer.stripe_id)
-          c.email = customer.email
-          c.account_balance = customer.account_balance if customer.account_balance.present?
-          c.business_vat_id = customer.business_vat_id if customer.business_vat_id.present?
-          c.description = customer.description
-          c.coupon = customer.coupon.stripe_id if customer.coupon.present?
+          c = populate_customer(c, customer)
           c.save
         rescue Stripe::StripeError => e
           customer.errors[:base] << e.message
@@ -18,6 +14,15 @@ module Tang
       end
 
       return customer
+    end
+
+    def self.populate_customer(stripe_customer, customer)
+      stripe_customer.email = customer.email
+      stripe_customer.account_balance = customer.account_balance if customer.account_balance.present?
+      stripe_customer.business_vat_id = customer.business_vat_id if customer.business_vat_id.present?
+      stripe_customer.description = customer.description
+      stripe_customer.coupon = customer.coupon.stripe_id if customer.coupon.present?
+      return stripe_customer
     end
   end
 end
