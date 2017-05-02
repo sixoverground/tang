104e539b26b1f879b21868465964a573ccbe0363
Craig Phares <craigphares@sixoverground.com>
2017-05-01 22:26:51 -0400

    revamped sub import

--- app/jobs/tang/import_subscriptions_job.rb
+++ app/jobs/tang/import_subscriptions_job.rb
@@ -8,8 +8,11 @@ module Tang
 
       # Import all Stripe subscriptions
       Stripe::Subscription.list(status: 'all').each do |stripe_subscription|
+        puts "looking at stripe subscription: #{stripe_subscription.id}"
         customer = Tang.customer_class.find_by(stripe_id: stripe_subscription.customer)
+        puts "customer: #{customer.id}" if customer.present?
         plan = Plan.find_by(stripe_id: stripe_subscription.plan.id)
+        puts "plan: #{plan.id}" if plan.present?
         if customer.present? && plan.present?
           subscription = Subscription.find_or_create_by(stripe_id: stripe_subscription.id) do |s|
             s.customer = customer
@@ -21,7 +24,8 @@ module Tang
             s.coupon = Coupon.find_by(stripe_id: stripe_subscription.discount.coupon.id) if stripe_subscription.discount.present?
             s.status = stripe_subscription.status
           end
-
+          puts "tang subscription: #{subscription.id}"
+          
           # Handle removed discounts
           if stripe_subscription.discount.nil?
             subscription.update(coupon: nil, coupon_start: nil)
