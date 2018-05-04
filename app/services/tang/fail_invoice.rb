f2e8fbc269deb71b876009778ad08b4bc809cbb0
Craig Phares <craigphares@gmail.com>
2018-05-04 14:24:54 -0400

    check for charge when failing an invoice

--- app/services/tang/fail_invoice.rb
+++ app/services/tang/fail_invoice.rb
@@ -4,8 +4,11 @@ module Tang
       invoice = Invoice.find_by(stripe_id: stripe_invoice.id)
 
       # create charge
-      stripe_charge = Stripe::Charge.retrieve(stripe_invoice.charge)
-      charge = Charge.from_stripe(stripe_charge, invoice)
+      charge = nil
+      if stripe_invoice.charge.present?
+        stripe_charge = Stripe::Charge.retrieve(stripe_invoice.charge)
+        charge = Charge.from_stripe(stripe_charge, invoice)
+      end
 
       # update subscription
       subscription = Subscription.find_by(stripe_id: stripe_invoice.subscription)
