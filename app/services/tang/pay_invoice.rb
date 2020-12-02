ff5a5038d1cd7f67beb1ff11aca2a00660904553
Craig Phares <craigphares@users.noreply.github.com>
2020-12-02 13:56:31 -0500

    Feature/rails6 (#21)
    * upgraded rails to 5.2
    
    * upgraded to rails 6
    
    * fixed missing stripe subscription when paying

--- app/services/tang/pay_invoice.rb
+++ app/services/tang/pay_invoice.rb
@@ -1,7 +1,15 @@
 module Tang
   class PayInvoice
     def self.call(stripe_invoice)
-      # invoice = Invoice.find_by(stripe_id: stripe_invoice.id)
+
+      # ensure the subscription exists
+      stripe_subscription = Stripe::Subscription.retrieve(stripe_invoice.subscription)
+      subscription = Subscription.find_by(stripe_id: stripe_invoice.subscription)
+      if subscription.nil?
+        subscription = Subscription.from_stripe(stripe_subscription)
+      end
+
+      # create the invoice
       invoice = Invoice.from_stripe(stripe_invoice)
 
       # create charge
@@ -11,20 +19,14 @@ module Tang
         charge = Charge.from_stripe(stripe_charge, invoice)
       end
 
-      # update subscription
-      stripe_subscription = Stripe::Subscription.retrieve(stripe_invoice.subscription)      
-      subscription = Subscription.find_by(stripe_id: stripe_invoice.subscription)
-
-      if subscription.present?
-        # update discount
-        if stripe_subscription.discount.nil?
-          subscription.update(coupon: nil, coupon_start: nil)
-        end
-
-        # update customer active until
-        customer = subscription.customer
-        customer.update_subscription_end(subscription)
+      # update discount
+      if stripe_subscription.discount.nil?
+        subscription.update(coupon: nil, coupon_start: nil)
       end
+
+      # update customer active until
+      customer = subscription.customer
+      customer.update_subscription_end(subscription)
 
       return charge
     end
