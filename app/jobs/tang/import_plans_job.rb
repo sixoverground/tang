37b8f8032f0a2bee6f82a841ece3dff2073a1c5f
Craig Phares <craigphares@users.noreply.github.com>
2022-11-16 16:21:03 -0500

    import name from stripe product

--- app/jobs/tang/import_plans_job.rb
+++ app/jobs/tang/import_plans_job.rb
@@ -4,22 +4,20 @@ module Tang
 
     def perform(starting_after = nil)
       # Do something later
-      stripe_plans = Stripe::Plan.list(limit: 100, starting_after: starting_after)
+      stripe_plans = Stripe::Plan.list(limit: 100, starting_after: starting_after, expand: ['data.product'])
       stripe_plans.each do |stripe_plan|
         Plan.find_or_create_by(stripe_id: stripe_plan.id) do |p|
           p.amount = stripe_plan.amount
           p.currency = stripe_plan.currency
           p.interval = stripe_plan.interval
           p.interval_count = stripe_plan.interval_count
-          p.name = stripe_plan.name
+          p.name = stripe_plan.product.name
           p.statement_descriptor = stripe_plan.statement_descriptor
           p.trial_period_days = stripe_plan.trial_period_days
         end
       end
 
-      if stripe_plans.has_more
-        Tang::ImportPlansJob.perform_now(stripe_plans.data.last.id)
-      end
+      Tang::ImportPlansJob.perform_now(stripe_plans.data.last.id) if stripe_plans.has_more
     end
   end
 end
