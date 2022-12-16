b806d5799042c5efe57f563fb599d799901100c3
aacohen <arielaades@gmail.com>
2022-12-16 11:10:35 -0500

    commented out for testing

--- config/initializers/stripe_event.rb
+++ config/initializers/stripe_event.rb
@@ -46,17 +46,18 @@ StripeEvent.configure do |events|
     invoice = event.data.object
     charge = Tang::PayInvoice.call(invoice)
     if charge.present?
-      if Tang.delayed_email
-        Tang::StripeMailer.customer_payment_succeeded(charge).deliver_later
-        if Tang.admin_payment_succeeded_enabled
-          Tang::StripeMailer.admin_payment_succeeded(charge).deliver_later
-        end
-      else
-        Tang::StripeMailer.customer_payment_succeeded(charge).deliver_now
-        if Tang.admin_payment_succeeded_enabled
-        Tang::StripeMailer.admin_payment_succeeded(charge).deliver_now
-        end
-      end
+      Tang::StripeMailer.admin_payment_succeeded(charge).deliver_now
+      # if Tang.delayed_email
+      #   Tang::StripeMailer.customer_payment_succeeded(charge).deliver_later
+      #   if Tang.admin_payment_succeeded_enabled
+      #     Tang::StripeMailer.admin_payment_succeeded(charge).deliver_later
+      #   end
+      # else
+      #   Tang::StripeMailer.customer_payment_succeeded(charge).deliver_now
+      #   if Tang.admin_payment_succeeded_enabled
+      #   Tang::StripeMailer.admin_payment_succeeded(charge).deliver_now
+      #   end
+      # end
     end
   end
 
