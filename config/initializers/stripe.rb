9a6302f7326592dba33f3d560635b25cbe348efb
Craig Phares <craigphares@users.noreply.github.com>
2023-03-15 11:30:01 -0400

    fallback to nil for missing stripe keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,2 @@
-Stripe.api_key = ENV.fetch('STRIPE_SECRET_KEY')
-StripeEvent.signing_secret = ENV.fetch('STRIPE_SIGNING_SECRET')
+Stripe.api_key = ENV.fetch('STRIPE_SECRET_KEY', nil)
+StripeEvent.signing_secret = ENV.fetch('STRIPE_SIGNING_SECRET', nil)
